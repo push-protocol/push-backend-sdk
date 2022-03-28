e0fd69a7e6aa16b09938b5d6b3bc1dc856e01470
OxHimanshu <OxHimanshu@users.noreply.github.com>
2022-03-28 07:13:36 +0000

    Apply formatting changes

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 // const BASE_URL = "http://localhost:4000/apis";
-const BASE_URL = "https://backend-prod.epns.io/apis";
+const BASE_URL = 'https://backend-prod.epns.io/apis';
 
 /**
  * A function used to make get requests throughout the entire application
@@ -9,21 +9,21 @@ const BASE_URL = "https://backend-prod.epns.io/apis";
  * @returns {Promise}
  */
 export const getReq = async (path: string) => {
-    try {
-      const response = await axios.get(BASE_URL + path);
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  };
+  try {
+    const response = await axios.get(BASE_URL + path);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 /**
  * A function used to make post requests to the backend
  * @param {String} path e.g /feeds/get_feeds, this represents the route of the enpoint to call
- * @param {Object} obj 
+ * @param {Object} obj
  * @returns {Promise}
  */
-export const postReq = async (path:string, obj:any) => {
+export const postReq = async (path: string, obj: any) => {
   try {
     const response = await axios.post(BASE_URL + path, obj, {
       headers: {
